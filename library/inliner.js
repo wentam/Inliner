4c1034eb7724b55c067250d81af910a2e3986466
wentam <wentam42@gmail.com>
2017-12-14 23:15:19 -0500

    structure

---
+++ library/inliner.js
@@ -0,0 +1,19 @@
+var inliner;
+
+(function () {
+  inliner = function() {
+
+  }
+
+  inliner.inlineStyles = function() {
+
+  }
+
+  inliner.inlineImages = function() {
+
+  }
+
+  inliner.inlineAll = function() {
+
+  }
+}());
